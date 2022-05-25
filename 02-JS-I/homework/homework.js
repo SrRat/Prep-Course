1aaf2fbba9cdf14ccd006c75f0526ffa99291ab0
SrRat <kxharu.no@gmail.com>
2022-05-24 21:12:21 -0500

    third commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola";
 const nuevoNum = "Chaoo";
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = "HolaMundo";
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
